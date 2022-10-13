cf9521d4ec129c87571baf1ae281796beedc4898
Nicolas Molina Monroy <hi@nicobytes.com>
2022-10-12 19:53:35 -0500

    save

--- src/challenges/js/multiply-elements/src/exercise.test.js
+++ src/challenges/js/multiply-elements/src/exercise.test.js
@@ -3,18 +3,18 @@ import { multiplyElements } from "./exercise";
 describe("tests", () => {
   it("should return [2,4,6]", () => {
     const array = [1, 2, 3];
-    const rta = runCode(array);
+    const rta = multiplyElements(array);
     expect(rta).toEqual([2, 4, 6]);
   });
 
   it("should return [0, 1, 4]", () => {
     const array = [0, -1, 2];
-    const rta = runCode(array);
+    const rta = multiplyElements(array);
     expect(rta).toEqual([0, -2, 4]);
   });
   it("should return []", () => {
     const array = [];
-    const rta = runCode(array);
+    const rta = multiplyElements(array);
     expect(rta).toEqual([]);
   });
 
