3350ebf15c8d94c1fb344e27d69133b6a3421600
RetaxMaster <retaxmaster@hotmail.com>
2022-10-17 16:26:08 -0500

    Adicion de la solucion para el challenge de Merge JSON

--- src/challenges/js/merge-json/src/exercise.js
+++ src/challenges/js/merge-json/src/exercise.js
@@ -1,3 +1,13 @@
-export function solution(json1, json2) {
+export function solution(json1 = {
+  name: "Mr. Michi",
+  food: "Pescado"
+}, json2 = {
+  age: 12,
+  color: "Blanco"
+}) {
   // Tu código aquí 👈
+  return {
+    ...json1,
+    ...json2
+  }
 }
