b5afe59bbf231cfcd7b07ad60516c21c25cb0815
RetaxMaster <48848312+RetaxMaster@users.noreply.github.com>
2022-10-19 07:46:11 -0500

    New Challenge: JSON Merge for Nuevo Curso de ECMAScript 6 (#1)
    * Adicion del reto Merge JSON
    
    * Fix for JSON Merge

---
+++ src/challenges/js/merge-json/src/index.js
@@ -0,0 +1,32 @@
+import "./styles.css";
+import { so, solutionlution } from "./exercise";
+
+(() => {
+  const json1 = {
+    name: "Atena",
+    food: "Leche"
+  };
+  const json2 = {
+    age: 2,
+    color: "Café"
+  }
+
+  document.getElementById("app").innerHTML = /*html*/`
+    <h1></h1>
+    <p>JSON 1: <code>${JSON.stringify(json1)}</code></p>
+    <p>JSON 2: <code>${JSON.stringify(json2)}</code></p>
+    <p><button id="btn">Run Code</button><p/>
+    <p><pre><code id="rta"></code></pre></p>
+  `;
+
+  const runBtn = document.getElementById('btn');
+  const rtaElement = document.getElementById('rta');
+
+  runBtn.addEventListener('click', () => {
+    try {
+      rtaElement.innerHTML = solution(json1, json2);
+    } catch (error) {
+      rtaElement.innerHTML = error;
+    }
+  });
+})();
