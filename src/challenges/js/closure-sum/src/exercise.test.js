3635ca3549c0a47dd773b285dc55b1614d211f72
Leonardo Espinoza <leoespinozidk@gmail.com>
2022-10-21 11:43:28 -0500

    Fix test closure-sum

--- src/challenges/js/closure-sum/src/exercise.test.js
+++ src/challenges/js/closure-sum/src/exercise.test.js
@@ -28,7 +28,7 @@ describe("tests", () => {
     expect(rta).toEqual(20);
   });
 
-  it("should return -20", () => {
+  it("should return -80", () => {
     const firstNum = -48;
     const secondNum = -32
     const rta = sumWithClosure(firstNum)(secondNum);
