0ccb06324be25fa52235eac939912ab8d782988a
Juan David Castro <me@juandc.co>
2022-10-23 22:21:52 -0500

    Solution to: Objeto to Array

--- src/challenges/js/jsmath-03/src/exercise.js
+++ src/challenges/js/jsmath-03/src/exercise.js
@@ -1,3 +1,24 @@
 export function solution(obj) {
   // Tu código aquí 👈
+
+  // Solution 1
+  // const usersArrayOfArrays = Object.entries(obj);
+
+  // const usersArray = usersArrayOfArrays.map(userArray => {
+  //   return {
+  //     id: userArray[0],
+  //     name: userArray[1],
+  //   };
+  // })
+
+  // return usersArray;
+
+  // Solution 2
+  const usersArray = [];
+
+  for (const [id, name] of Object.entries(obj)) {
+    usersArray.push({ id, name });
+  }
+
+  return usersArray;
 }
