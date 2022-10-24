36827cc5d126e658534b528d9281483391e19525
RetaxMaster <48848312+RetaxMaster@users.noreply.github.com>
2022-10-24 17:03:02 -0500

    New Challenge: ID Generator (#15)
    * Adicion del reto Merge JSON
    
    * Fix for JSON Merge
    
    * Fix for Merge JSON
    
    * New challenge: Fake API Module for Curso de ECMAScript
    
    * New Challenge: ID Generator

---
+++ src/challenges/js/id-generator/src/index.js
@@ -0,0 +1,31 @@
+import "./styles.css";
+import { getId } from "./exercise";
+
+(() => {
+
+  document.getElementById("app").innerHTML = `
+    <h1>Generador de ID</h1>
+    <p><button id="btn">Generate 10 ID</button><p/>
+    <p><pre><code id="rta"></code></pre></p>
+  `;
+
+  const runBtn = document.getElementById('btn');
+  const rtaElement = document.getElementById('rta');
+
+  runBtn.addEventListener('click', () => {
+
+    try {
+
+      const genId = getId();
+      const generatedId = [];
+
+      for (let i = 0; i < 10; i++)
+        generatedId.push(genId.next().value);
+
+      rtaElement.innerHTML = JSON.stringify(generatedId);
+
+    } catch (error) {
+      rtaElement.innerHTML = error;
+    }
+  });
+})();
