1a5d58afd09cee9472133500efd89b1a622a88e7
Juan David Castro <me@juandc.co>
2022-10-25 10:22:47 -0500

    Just 1 solution

--- src/challenges/js/jsmath-03/src/exercise.js
+++ src/challenges/js/jsmath-03/src/exercise.js
@@ -1,24 +1,12 @@
 export function solution(obj) {
-  // Tu código aquí 👈
+  const usersArrayOfArrays = Object.entries(obj);
 
-  // Solution 1
-  // const usersArrayOfArrays = Object.entries(obj);
-
-  // const usersArray = usersArrayOfArrays.map(userArray => {
-  //   return {
-  //     id: userArray[0],
-  //     name: userArray[1],
-  //   };
-  // })
-
-  // return usersArray;
-
-  // Solution 2
-  const usersArray = [];
-
-  for (const [id, name] of Object.entries(obj)) {
-    usersArray.push({ id, name });
-  }
+  const usersArray = usersArrayOfArrays.map(userArray => {
+    return {
+      id: userArray[0],
+      name: userArray[1],
+    };
+  })
 
   return usersArray;
 }
