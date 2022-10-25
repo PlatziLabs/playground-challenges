1d2adfb0ccd949743c4a95ead9150f65e5923078
Juan David Castro <me@juandc.co>
2022-10-25 13:53:32 -0500

    New Challenge: Retorna el tipo, Curso Básico de JavaScript

---
+++ src/challenges/js/jsbasico-01/src/exercise.js
@@ -0,0 +1,5 @@
+/* ¡Hola, Platzinauta! Por favor ignora la palabra export que agregamos antes de la función solution, la necesitamos para ejecutar las pruebas de tu código. Por ahora no te preocupes por eso, más adelante en tu ruta de aprendizaje aprenderás a profundidad cómo funciona. ¡Mucha suerte en tu reto! */
+
+export function solution(valor) {
+  // Escribe tu código aquí 👈
+}
