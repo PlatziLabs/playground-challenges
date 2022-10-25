c85362a65e391db093b03d6c30245b92084b2051
Nicolas Molina Monroy <hi@nicobytes.com>
2022-10-25 12:05:42 -0400

    :bug: Fix error

--- src/challenges/js/jsmath-02/src/exercise.js
+++ src/challenges/js/jsmath-02/src/exercise.js
@@ -1,9 +1,3 @@
 export function solution(users, id) {
-  const userWithID = users.find(user => user.id == id);
-
-  if (userWithID) {
-    return userWithID.name;
-  } else {
-    return false;
-  }
+  // Tu código aquí 👈
 }
