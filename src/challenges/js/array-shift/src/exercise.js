3caec468060d4bd385a5ab2b7d4f8a5ea864eaf4
Leonardo Espinoza <leoespinozidk@gmail.com>
2022-10-28 09:41:46 -0500

    New challenge: array-shift, Curso de Estructuras de Datos con JavaScript (#58)
    * save
    
    * save
    
    * save
    
    * Solution for Merge JSON (#2)
    
    * save
    
    * Adicion del reto Merge JSON
    
    * Adicion de la solucion para el challenge de Merge JSON
    
    * Fix for JSON Merge
    
    * Fix Solution for Merge JSON
    
    Co-authored-by: Nicolas Molina Monroy <hi@nicobytes.com>
    
    * save
    
    * New challenge: closure-sum for Curso de Closures y Scope en JavaScript
    
    * Fix closure-sum
    
    * Fix test closure-sum
    
    * New challenge: array-shift, Curso de Estructuras de Datos con JavaScript
    
    Co-authored-by: Nicolas Molina Monroy <hi@nicobytes.com>
    Co-authored-by: RetaxMaster <48848312+RetaxMaster@users.noreply.github.com>

---
+++ src/challenges/js/array-shift/src/exercise.js
@@ -0,0 +1,14 @@
+import { MyArray } from "./MyArray";
+
+export class BetterArray extends MyArray {
+  constructor(){
+    super()
+    this.length = 0
+    this.data = {}
+  }
+
+  shift(){
+    // Tu código aquí 👈
+    
+  }
+}
