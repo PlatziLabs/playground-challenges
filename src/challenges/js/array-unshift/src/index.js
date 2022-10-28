920189af49bca9f77ac734547e125b4a83c62e22
Leonardo Espinoza <leoespinozidk@gmail.com>
2022-10-28 09:32:08 -0500

    New challenge: array-unshift, Curso de Estructuras de Datos con JavaScript (#55)
    * save
    
    * save
    
    * save
    
    * Solution for Merge JSON (#2)
    
    * save
    
    * Adicion del reto Merge JSON
    
    * Adicion de la solucion para el challenge de Merge JSON
    
    * Fix for JSON Merge
    
    * Fix Solution for Merge JSON
    
    Co-authored-by: Nicolas Molina Monroy <hi@nicobytes.com>
    
    * save
    
    * New challenge: closure-sum for Curso de Closures y Scope en JavaScript
    
    * Fix closure-sum
    
    * Fix test closure-sum
    
    * New challenge: array-unshift, Curso de Estructuras de Datos con JavaScript
    
    Co-authored-by: Nicolas Molina Monroy <hi@nicobytes.com>
    Co-authored-by: RetaxMaster <48848312+RetaxMaster@users.noreply.github.com>

---
+++ src/challenges/js/array-unshift/src/index.js
@@ -0,0 +1,28 @@
+import "./styles.css";
+import { MyArray } from "./exercise";
+
+(() => {
+  const myArray = new MyArray();
+
+  document.getElementById("app").innerHTML = `
+    <h1>Test your unshift implementation</h1>
+    <p>myArray: <code id="rta" >${JSON.stringify(myArray.data)}</code></p>
+    <input type="text" placeholder="Write something" id="input" >
+    <button id="btn">Add to array</button>
+    <p id="err"></p>
+  `;
+
+  const runBtn = document.getElementById('btn');
+  const err = document.getElementById("err")
+  const rtaElement = document.getElementById('rta');
+  const input = document.getElementById("input")
+
+  runBtn.addEventListener('click', () => {
+    try {
+      myArray.unshift(input.value)
+      rtaElement.innerHTML = JSON.stringify(myArray);
+    } catch (error) {
+      err.innerHTML = error;
+    }
+  });
+})();
