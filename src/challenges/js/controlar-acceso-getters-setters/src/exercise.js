a56ee1b489f4988bb124f3688cf5efb4f2ea9496
Erik Ochoa <elyager@gmail.com>
2022-10-28 07:17:36 -0700

    New challenge: getters-setters for Curso Básico de POO con JS (#57)

---
+++ src/challenges/js/controlar-acceso-getters-setters/src/exercise.js
@@ -0,0 +1,17 @@
+export class Course {
+  constructor({
+    name,
+    classes = [],
+  }) {
+    this.name = name;
+    this.classes = classes;
+  }
+
+  get name() {
+    // Tu código aquí 👈
+  }
+
+  set name(nuevoNombrecito) {
+    // Tu código aquí 👈
+  }
+}
