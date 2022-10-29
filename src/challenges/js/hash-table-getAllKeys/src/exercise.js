a6d8a92f8837bd09822e125e31dfebd3ac220c92
Leonardo Espinoza <leoespinozidk@gmail.com>
2022-10-28 19:52:01 -0500

    Solution to hash-table-getAllKeys, Curso de estructuras de datos con JS  (#69)
    * New challenge: closure-sum for Curso de Closures y Scope en JavaScript
    
    * Fix closure-sum
    
    * Fix test closure-sum
    
    * fix title to array-shift, curso de estructuras de datos con JS
    
    * New challenge: hash-table-getAllKeys, Curso de estructuras de datos con JS
    
    * Solution to hash-table-getAllKeys, Curso de estructuras de datos con JS
    
    Co-authored-by: Nicolas Molina Monroy <hi@nicobytes.com>

--- src/challenges/js/hash-table-getAllKeys/src/exercise.js
+++ src/challenges/js/hash-table-getAllKeys/src/exercise.js
@@ -8,6 +8,21 @@ export class Solution extends HashTable {
 
 
   getAllKeys(){
-    // Tu código aquí 👈
+    let keys = []
+    for (let i = 0; i < this.data.length; i++) {
+      const element = this.data[i]
+      if(element){
+        if(element.length > 1){
+          for (let j = 0; j < element.length; j++) {
+            const item = element[j];
+            keys = [...keys, item[0]]
+          }
+        }else{
+          keys = [...keys, element[0][0]]
+        }
+      }
+    }
+
+    return keys
   }
 }
