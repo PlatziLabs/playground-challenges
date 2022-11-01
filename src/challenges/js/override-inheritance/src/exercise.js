316a3c4cd9217a18ee962b46fa9318035f60c819
Erik Ochoa <elyager@gmail.com>
2022-11-01 13:39:34 -0700

    Fix override inheritance (#77)
    * fix: simplify structure of classes
    
    * fix: wrong markdown rendering
    
    Co-authored-by: Nicolas Molina Monroy <hi@nicobytes.com>

--- src/challenges/js/override-inheritance/src/exercise.js
+++ src/challenges/js/override-inheritance/src/exercise.js
@@ -1,64 +1,12 @@
-<<<<<<< HEAD
-class Student {
-  constructor({
-    name,
-    email,
-    username,
-    twitter = undefined,
-    instagram = undefined,
-    facebook = undefined,
-    approvedCourses = [],
-    learningPaths = [],
-  }) {
-    this.name = name;
-    this.email = email;
-    this.username = username;
-    this.socialMedia = {
-      twitter,
-      instagram,
-      facebook,
-    };
-    this.approvedCourses = approvedCourses;
-    this.learningPaths = learningPaths;
-  }
 
-  publicarComentario(commentContent) {
-    const comment = new Comment({
-      content: commentContent,
-      studentName: this.name,
-    });
-    return comment.publicar();
-  }
-}
+import { Student, Comment } from "./utilityClasses";
 
-class Comment {
-  constructor({
-    content,
-    studentName,
-    studentRole = "estudiante",
-  }) {
-    this.content = content;
-    this.studentName = studentName;
-    this.studentRole = studentRole;
-    this.likes = 0;
-  }
+export class TeacherStudent extends Student {
 
-  publicar() {
-    const comentario = {
-      studentName: this.studentName + " (" + this.studentRole + ")",
-      likes: this.likes + " likes",
-      content: this.content
-    }
-    return comentario
-  }
-}
-
-class TeacherStudent extends Student {
   constructor(parentProps, skills) {
     super(parentProps);
     this.skills = skills;
   }
-
 
   publicarComentario(commentContent) {
     const comment = new Comment({
@@ -68,14 +16,4 @@ class TeacherStudent extends Student {
     });
     return comment.publicar()
   }
-}
-
-export {
-  Student,
-  TeacherStudent
-=======
-import { Student, Comment } from "./utilityClasses";
-export class TeacherStudent {
-  // Tu código aquí 👈
->>>>>>> master
 }
