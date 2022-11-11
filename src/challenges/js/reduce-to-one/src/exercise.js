5404a8ec7034d5d0dd4bf363df58ed396f680074
Erik Ochoa <elyager@gmail.com>
2022-11-10 18:19:42 -0700

    New challenge: reduce-to-one for Curso de Desafíos de pensamiento lógico

--- src/challenges/js/reduce-to-one/src/exercise.js
+++ src/challenges/js/reduce-to-one/src/exercise.js
@@ -1,17 +1,3 @@
 export function solution(num) {
   // Tu código aquí 👈
-  let count = 0;
-  while (num > 1) {
-    count++;
-    if (num % 2 === 0) {
-      num = Math.floor(num / 2);
-    }
-    else if (num % 4 === 1 || num === 3) {
-      num -= 1;
-    }
-    else {
-      num += 1;
-    }
-  }
-  return count;
 }
