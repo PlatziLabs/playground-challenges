f970d4bbdfc8e1eafa1842efb764fbc5956d8a8b
Erik Ochoa <elyager@gmail.com>
2022-11-15 15:21:13 -0700

    Challenge/get multiplier factor solution (#90)
    * Challenge/get multiplier factor (#84)
    
    * New challenge: get-multiplier-factor for Desafíos de pensamiento lógico
    
    * fix: improve README and view
    
    * Add solution: get-multiplier-factor for Desafíos de pensamiento lógico

---
+++ src/challenges/js/get-multiplier-factor/src/index.js
@@ -0,0 +1,23 @@
+import "./styles.css";
+import { solution } from "./exercise";
+
+(() => {
+  const defaultArray = [27, 36, 45, 54, 63, 72]
+  document.getElementById("app").innerHTML = `
+    <h1>Obtén el factor multiplicador</h1>
+    <p>Array de prueba:<code> ${JSON.stringify(defaultArray)} </code></p>
+    <p><button id="btn">Run Code</button><p/>
+    <p><pre><code id="rta"></code></pre></p>
+  `;
+
+  const runBtn = document.getElementById('btn');
+  const rtaElement = document.getElementById('rta');
+
+  runBtn.addEventListener('click', () => {
+    try {
+      rtaElement.innerHTML = JSON.stringify(solution(defaultArray));
+    } catch (error) {
+      rtaElement.innerHTML = error;
+    }
+  });
+})();
