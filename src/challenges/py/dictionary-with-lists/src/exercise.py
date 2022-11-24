5dea1072280bfdc7e0aa1c1a11524ad55fa48b58
Leonardo Espinoza <leoespinozidk@gmail.com>
2022-11-24 08:10:29 -0600

    New challenge: dictionary_with_list, Curso de Fundamentos de Python (#96)
    * save
    
    * save
    
    * save
    
    * Solution for Merge JSON (#2)
    
    * save
    
    * Adicion del reto Merge JSON
    
    * Adicion de la solucion para el challenge de Merge JSON
    
    * Fix for JSON Merge
    
    * Fix Solution for Merge JSON
    
    Co-authored-by: Nicolas Molina Monroy <hi@nicobytes.com>
    
    * save
    
    * New challenge: closure-sum for Curso de Closures y Scope en JavaScript
    
    * Fix closure-sum
    
    * Fix test closure-sum
    
    * fix title to array-shift, curso de estructuras de datos con JS
    
    * Fix readme guide for hash table delete
    
    * New challenge: dictionary_with_list, Curso de Fundamentos de Python
    
    * Update readme
    
    Co-authored-by: Nicolas Molina Monroy <hi@nicobytes.com>
    Co-authored-by: RetaxMaster <48848312+RetaxMaster@users.noreply.github.com>

---
+++ src/challenges/py/dictionary-with-lists/src/exercise.py
@@ -0,0 +1,16 @@
+def dictionary_with_lists(names):
+    students = {
+        'school': "Platzi",
+        'names': "",
+        'first_student': "",
+        'last_student': "",
+        'count': 0
+    }
+
+    # Tu código aquí
+
+    return students
+
+
+response = dictionary_with_lists(["Pepe", "Luis"])
+print(response)
