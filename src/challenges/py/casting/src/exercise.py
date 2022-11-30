a330dc1f11aeba8b03c01abadab02be4c174f275
Nicolas Molina Monroy <hi@nicobytes.com>
2022-11-30 02:03:26 -0400

    :construction: work

--- src/challenges/py/casting/src/exercise.py
+++ src/challenges/py/casting/src/exercise.py
@@ -6,5 +6,5 @@ age = int(input('¿Cuál es tu edad? => '))
 print(age)
 
 
-template = f"Hola mi nombre es {name} {last_name} y tengo {age} años y en 10 años tendré {age + 10} años"
+template = f"Hola mi nombre es {name} {last_name}, tengo {age} años y en 10 años tendré {age + 10} años"
 print(template)
