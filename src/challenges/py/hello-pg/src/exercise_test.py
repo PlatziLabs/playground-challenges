0db60ef66b3f9c4d4e8c9f17c2e1de0b1940cddd
Nicolas Molina Monroy <hi@nicobytes.com>
2022-11-29 22:34:04 -0400

    :bug: Fix error

--- src/challenges/py/hello-pg/src/exercise_test.py
+++ src/challenges/py/hello-pg/src/exercise_test.py
@@ -1,5 +1,15 @@
+from importlib import reload, import_module
+import shutil
+import exercise
+
+def reload_module(name):
+  module = import_module(name)
+  shutil.rmtree("__pycache__", ignore_errors=True)
+  reload(module)
+  return module
+
 def test_print_hello(capfd):
-    import exercise
+    reload_module('exercise')
     expected_str = "Te doy la bienvenida al Coding Playground\nMi primer print\n30\n"
     out, error = capfd.readouterr()
     assert out  == expected_str
