ecffed5ccb9c89e41e447da7fbc4273a73b9172e
Nicolas Molina Monroy <hi@nicobytes.com>
2022-12-08 07:52:49 -0400

    :passport_control: work

--- src/challenges/py/inmutable-remove/src/exercise_test.py
+++ src/challenges/py/inmutable-remove/src/exercise_test.py
@@ -9,19 +9,19 @@ def reload_module(name):
   return module
 
 def test_numbers():
-    reload_module('exercise')
+    module = reload_module('exercise')
     tests_list = [1, 2, 3]
-    rta = multiply_numbers(tests_list)
+    rta = module.multiply_numbers(tests_list)
     assert rta == [2, 4, 6]
 
 def test_negative_numbers():
-    reload_module('exercise')
+    module = reload_module('exercise')
     tests_list = [0, -1, 2]
-    rta = multiply_numbers(tests_list)
+    rta = module.multiply_numbers(tests_list)
     assert rta == [0, -2, 4]
 
 def test_empty_list():
-    reload_module('exercise')
+    module = reload_module('exercise')
     tests_list = []
-    rta = multiply_numbers(tests_list)
+    rta = module.multiply_numbers(tests_list)
     assert rta == []
