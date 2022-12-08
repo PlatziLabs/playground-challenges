b5a246434dc12faff293a2b5df3c0d9e22478d6c
Nicolas Molina Monroy <hi@nicobytes.com>
2022-12-08 07:33:30 -0400

    :construction: work

--- src/challenges/py/reduce-count/src/exercise_test.py
+++ src/challenges/py/reduce-count/src/exercise_test.py
@@ -1,4 +1,14 @@
 from exercise import counter_numbers
+from importlib import reload, import_module
+import shutil
+
+def reload_module(name):
+  module = import_module(name)
+  shutil.rmtree("__pycache__", ignore_errors=True)
+  reload(module)
+  return module
+
+reload_module('exercise')
 
 def test_case_1():
     tests_list = [1, 2, 2, 3, 3, 3]
