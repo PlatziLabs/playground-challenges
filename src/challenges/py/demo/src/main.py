1794aaeed4fe8e77256d6907366f758f684a69ef
Nicolas Molina Monroy <hi@nicobytes.com>
2022-12-15 15:26:50 -0400

    :construction: work

---
+++ src/challenges/py/demo/src/main.py
@@ -0,0 +1,9 @@
+import csv
+
+def read_csv(path):
+   # Tu código aquí 👇
+   total = 0
+   return total
+
+response = read_csv('./data.csv')
+print(response)
---
+++ src/challenges/py/demo/src/main_test.py
@@ -0,0 +1,33 @@
+from main import read_csv
+from importlib import reload, import_module
+import shutil
+from unittest.mock import patch, mock_open
+
+def reload_module(name):
+  module = import_module(name)
+  shutil.rmtree("__pycache__", ignore_errors=True)
+  reload(module)
+  return module
+
+def test_case_1():
+    in_mem_csv = "A,10\nB,20\nC,30\nD,40\n"
+    with patch("builtins.open", mock_open(read_data=in_mem_csv)):
+        module = reload_module('main')
+        rta = module.read_csv('./data.csv')
+        assert rta == 100
+
+def test_case_2():
+    in_mem_csv = "A,10\nB,10\nC,10\nD,10\n"
+    with patch("builtins.open", mock_open(read_data=in_mem_csv)):
+        module = reload_module('main')
+        rta = module.read_csv('./data.csv')
+        assert rta == 40
+        
+def test_case_3():
+    in_mem_csv = "A,1\nB,1\nC,1\nD,1\n"
+    with patch("builtins.open", mock_open(read_data=in_mem_csv)):
+        module = reload_module('main')
+        rta = module.read_csv('./data.csv')
+        assert rta == 4
+        
+   
