e3c9f6a07c434cc7c98b969ad8a28dc26ec984b0
Nicolas Molina Monroy <hi@nicobytes.com>
2022-12-20 12:00:57 -0400

    :construction: work

--- src/challenges/py/list-comprehension/src/main_test.py
+++ src/challenges/py/list-comprehension/src/main_test.py
@@ -7,9 +7,15 @@ def reload_module(name):
   reload(module)
   return module
 
-def test_many_countries(capfd):
+def test_v1(capfd):
     reload_module('main')
     str_v1 = "v1 => [16, 10, 34]"
+    out, error = capfd.readouterr()
+    assert str_v1 in out
+
+
+def test_v2(capfd):
+    reload_module('main')
     str_v2 = "v2 => [16, 10, 34]"
     out, error = capfd.readouterr()
-    assert str_v1 in out and str_v2 in out
+    assert str_v2 in out
