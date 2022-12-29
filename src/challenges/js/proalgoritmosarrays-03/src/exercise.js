4159fdb7eed6b523035fb608124672d5f30f8f32
Juan David Castro <me@juandc.co>
2022-12-29 14:36:20 -0500

    Solution to CWMW, Curso de Algoritmos Avanzados: Arrays

--- src/challenges/js/proalgoritmosarrays-03/src/exercise.js
+++ src/challenges/js/proalgoritmosarrays-03/src/exercise.js
@@ -1,3 +1,18 @@
 export function maxArea(alturas) {
-  // Tu código aquí 👈
+  let izquierda = 0;
+  let derecha = alturas.length - 1;
+  let areaMaxima = 0;
+
+  while (izquierda < derecha) {
+    let areaActual = (derecha - izquierda) * Math.min(alturas[izquierda], alturas[derecha]);
+    areaMaxima = Math.max(areaMaxima, areaActual);
+
+    if (alturas[izquierda] < alturas[derecha]) {
+      izquierda += 1;
+    } else {
+      derecha -= 1;
+    }
+  }
+
+  return areaMaxima;
 }
