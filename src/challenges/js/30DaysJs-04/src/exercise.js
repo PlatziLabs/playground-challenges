65b953a21189e837b8d0d8b2cc89e97c2e89bb5a
Leonardo Espinoza <leoespinozidk@gmail.com>
2023-01-03 14:02:21 -0600

    Solution to printTriangle, Challenge 30 dias de JS

---
+++ src/challenges/js/30DaysJs-04/src/exercise.js
@@ -0,0 +1,11 @@
+export function printTriangle(size, character) {
+  const triangle = []
+	for(let i = 1; i <= size; i++){
+	    let spaces = " ".repeat(size - i)
+	    let characters = character.repeat(i)
+	    let figure = `${spaces}${characters}` 
+	    triangle.push(figure)
+	}
+	
+	return triangle.join("\n")
+}
