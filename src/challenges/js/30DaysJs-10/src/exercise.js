187aa4fbc396f2b06ca5ca32401251a467f56da3
Leonardo Espinoza <leoespinozidk@gmail.com>
2023-01-16 16:19:15 -0600

    Solution to taskPlanner, Challenge 30 dias de JS (#130)
    * Solution to taskPlanner, Challenge 30 dias de JS
    
    * sync branch
    
    Co-authored-by: Nicolas Molina Monroy <hi@nicobytes.com>

--- src/challenges/js/30DaysJs-10/src/exercise.js
+++ src/challenges/js/30DaysJs-10/src/exercise.js
@@ -1,3 +1,56 @@
 export function createTaskPlanner() {
-  // Tu código aquí 👈
+  let tasks = [];
+
+  return {
+    addTask(task) {
+      task.completed = false;
+      tasks.push(task);
+    },
+
+    removeTask(value) {
+      if (typeof value === "number") {
+        tasks = tasks.filter((task) => task.id !== value);
+      } else {
+        tasks = tasks.filter((task) => task.name !== value);
+      }
+    },
+
+    getTasks() {
+      return tasks;
+    },
+
+    getPendingTasks() {
+      return tasks.filter((task) => !task.completed);
+    },
+
+    getCompletedTasks() {
+      return tasks.filter((task) => task.completed);
+    },
+
+    markTaskAsCompleted(value) {
+      let index;
+
+      if (typeof value === "number") {
+        index = tasks.findIndex((task) => task.id === value);
+      } else {
+        index = tasks.findIndex((task) => task.name === value);
+      }
+
+      tasks[index].completed = true;
+    },
+
+    getSortedTasksByPriority() {
+      const sortedTasks = [...tasks].sort((a, b) => a.priority - b.priority);
+      return sortedTasks;
+    },
+
+    filterTasksByTag(tag) {
+      return tasks.filter((task) => task.tags.includes(tag));
+    },
+
+    updateTask(taskId, updates) {
+      const index = tasks.findIndex((task) => task.id === taskId);
+      tasks[index] = { ...tasks[index], ...updates };
+    },
+  };
 }
