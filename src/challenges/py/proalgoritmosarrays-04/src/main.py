6aac522f7d6bde9b82d14b5d615f5d83c34d94fd
Juan David Castro <me@juandc.co>
2023-01-16 19:51:20 -0500

    New Challenge: LSWRC, Curso de Algoritmos Avanzados: Arrays

---
+++ src/challenges/py/proalgoritmosarrays-04/src/main.py
@@ -0,0 +1,13 @@
+def lengthOfLongestSubstring(s):
+   # Tu código aquí 👇
+   return s
+
+
+response = lengthOfLongestSubstring("abcabcbb")
+print(response)
+
+response2 = lengthOfLongestSubstring("jdkafnlcdsalkxcmpoiuytfccv")
+print(response2)
+
+response3 = lengthOfLongestSubstring("lavacalolatienecabezapeloycola")
+print(response3)
