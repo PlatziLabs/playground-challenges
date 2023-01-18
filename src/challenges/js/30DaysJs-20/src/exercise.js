e90cf3428a451be9eddbe91ac312596486544649
Leonardo Espinoza <leoespinozidk@gmail.com>
2023-01-18 16:04:46 -0600

    remove solution

--- src/challenges/js/30DaysJs-20/src/exercise.js
+++ src/challenges/js/30DaysJs-20/src/exercise.js
@@ -1,14 +1,3 @@
 export function arrayModified() {
-  Array.prototype.myFilter = function (func) {
-    const newArray = [];
-
-    for (let index = 0; index < this.length; index++) {
-      const element = this[index];
-      if (func(element)) {
-        newArray.push(element);
-      }
-    }
-
-    return newArray;
-  };
+  // Tu código aquí 👈
 }
