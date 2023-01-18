a6c59b4968738939b9d25fe7969b698f02c08b19
Juan David Castro <me@juandc.co>
2023-01-18 16:49:12 -0500

    Solution to Sistema de Navegación, Secretos de JavaScript (#136)
    * New Challenge: Sistema Navegacion, Secretos JavaScript
    
    * Solution to Sistema Navegacion, Secretos JavaScript
    
    Co-authored-by: Nicolas Molina Monroy <hi@nicobytes.com>

--- src/challenges/js/secretosjs-02/src/exercise.js
+++ src/challenges/js/secretosjs-02/src/exercise.js
@@ -4,36 +4,40 @@ export function simulador(astronaut, spaceShip, direction) {
 }
 
 export class Astronaut {
+  #_spaceShipKey;
+
   constructor({ name }) {
     this.name = name;
-    this._spaceShipKey = undefined;
+    this.#_spaceShipKey = undefined;
   }
 
   setAccessKey(accessKey) {
-    this._spaceShipKey = accessKey;
+    this.#_spaceShipKey = accessKey;
   }
 
   navigate(spaceShip, direction) {
-    spaceShip.navigator(direction, { accessKey: this._spaceShipKey });
+    spaceShip.navigator(direction, { accessKey: this.#_spaceShipKey });
   }
 }
 
 export class SpaceShip {
+  #_key;
+
   constructor({ key }) {
-    this._key = key;
+    this.#_key = key;
     this._movements = [];
   }
-  
+
   getAccessKey(astronaut) {
     const isAstronaut = astronaut instanceof Astronaut;
 
     if (isAstronaut) {
-      astronaut.setAccessKey(this._key);
+      astronaut.setAccessKey(this.#_key);
     }
   }
 
   navigator(direction, { accessKey }) {
-    if (this._key == accessKey) {
+    if (this.#_key == accessKey) {
       this._movements.push(direction)
     } else {
       this._movements.push("Incorrect Access Key");
