b119a1459f63ded6fbbd82c3ca82983d7ea5cdcb
Leonardo Espinoza <leoespinozidk@gmail.com>
2023-01-19 16:30:25 -0600

    remove solution

--- src/challenges/js/30DaysJs-22/src/exercise.js
+++ src/challenges/js/30DaysJs-22/src/exercise.js
@@ -1,36 +1,13 @@
 import { Product } from "./product";
 
-export class Article extends Product {
-  addToCart() {
-    return `Agregando ${this.quantity} unidades del articulo ${this.name} al carrito`;
-  }
+export class Article {
+  // Tu código aquí 👈
 }
 
-export class Service extends Product {
-  addToCart() {
-    return `Agregando el servicio ${this.name} al carrito`;
-  }
+export class Service {
+  // Tu código aquí 👈
 }
 
 export class Cart {
-  constructor() {
-    this.products = [];
-  }
-
-  addProduct(product) {
-    product.addToCart();
-    this.products.push(product);
-  }
-
-  deleteProduct(product) {
-    this.products = this.products.filter((item) => item.name !== product.name);
-  }
-
-  calculateTotal() {
-    const total = this.products.reduce(
-      (sum, currentItem) => currentItem.price * currentItem.quantity + sum,
-      0
-    );
-    return total;
-  }
+  // Tu código aquí 👈
 }
