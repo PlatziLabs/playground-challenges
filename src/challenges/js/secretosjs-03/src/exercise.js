be56df2f49d197e26f7aba65514445f75003f966
Juan David Castro <me@juandc.co>
2023-01-18 23:03:07 -0500

    Fix solution to secretosjs: from #privateFields to constructor variables (#144)
    * Change #privateFields to constructor variables
    
    * Motor & History: #privateFields to constructor variables

--- src/challenges/js/secretosjs-03/src/exercise.js
+++ src/challenges/js/secretosjs-03/src/exercise.js
@@ -1,55 +1,38 @@
 export class Motor {
-  #propulsionTo;
-  #status = "off";
-  #history;
+  constructor(propulsionTo, historyInstance) {
+    let status = "off"
+    let history = historyInstance;
 
-  constructor(propulsionTo, history) {
-    this.#propulsionTo = propulsionTo;
-    this.#history = history;
-  }
-
-  get status() {
-    return this.#status;
-  }
-
-  #setStatus(newStatus) {
-    this.#status = newStatus;
-    this.#history.state = {
-      propulsionTo: this.#propulsionTo,
-      status: newStatus,
+    this.getStatus = () => status;
+    let setStatus = (newStatus) => {
+      status = newStatus;
+      history.setState({
+        status: newStatus,
+        propulsionTo,
+      });
     };
-  }
 
-  turnOn() {
-    this.#setStatus("on");
-  }
-
-  turnOff() {
-    this.#setStatus("off");
-  }
-
-  get history() {
-    return this.#history.fullState;
+    this.turnOn = () => setStatus("on");
+    this.turnOff = () => setStatus("off");
+    
+    this.getHistory = () => history.getFullState();
   }
 }
 
 export class History {
-  #history = [];
+  constructor() {
+    let history = [];
 
-  get state() {
-    return this.#history[this.#history.length - 1];
-  }
+    this.getState = () => history[history.length - 1];
+    this.getFullState = () => history;
 
-  set state(newState) {
-    const stateStr = JSON.stringify(this.state);
-    const newStateStr = JSON.stringify(newState);
+    this.setState = (newState) => {
+      const stateStr = JSON.stringify(this.getState());
+      const newStateStr = JSON.stringify(newState);
 
-    if (stateStr != newStateStr) {
-      this.#history.push(newState);
-    }
-  }
-
-  get fullState() {
-    return this.#history;
+      if (stateStr != newStateStr) {
+        history.push(newState);
+      }
+    };
   }
 }
