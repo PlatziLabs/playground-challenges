5e7be1dc160f93cccd6ab89b1addf727a70f80f5
Leonardo Espinoza <leoespinozidk@gmail.com>
2023-01-19 16:29:10 -0600

    New challenge: cart system, challenge 30 dias de js

---
+++ src/challenges/js/30DaysJs-22/src/product.js
@@ -0,0 +1,14 @@
+export class Product {
+  // No debes editar este archivo ❌
+  constructor(name, price, quantity) {
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+  }
+
+  addToCart() {
+    throw new Error(
+      "La lógica de este método debe ser implementada por las clases hijas"
+    );
+  }
+}
