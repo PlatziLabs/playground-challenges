0d0d731ffa7ee307fb723828087d3f90f3d249d1
Leonardo Espinoza <leoespinozidk@gmail.com>
2023-01-26 17:39:33 -0600

    new challenge: pay system, challenge 30 dias de js

---
+++ src/challenges/js/30DaysJs-25/src/exercise.js
@@ -0,0 +1,19 @@
+export class Pay {
+  // Tu código aquí 👈
+}
+
+export class Card {
+  // Tu código aquí 👈
+}
+
+export class PayPal {
+  // Tu código aquí 👈
+}
+
+export class Cash {
+  // Tu código aquí 👈
+}
+
+export function processPay(method, quantity) {
+  // Tu código aquí 👈
+}
