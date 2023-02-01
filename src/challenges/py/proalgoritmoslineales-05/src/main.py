6ef321090a4d06ffc791492077475af8c5ee4925
Juan David Castro <me@juandc.co>
2023-02-01 13:54:23 -0500

    Solution to lParenthesis, Algoritmos Avanzados: Lineales (post class)

--- src/challenges/py/proalgoritmoslineales-05/src/main.py
+++ src/challenges/py/proalgoritmoslineales-05/src/main.py
@@ -1,6 +1,17 @@
 def isValid(s):
-   # Tu código aquí 👇
-   return True
+   open = 0
+   close = 0
+
+   for x in s:
+      if x == '(':
+         open += 1
+      if x == ')':
+         if close < open:
+            close += 1
+         else:
+            return False
+   
+   return open == close
    
 
 response = isValid('()()()')
