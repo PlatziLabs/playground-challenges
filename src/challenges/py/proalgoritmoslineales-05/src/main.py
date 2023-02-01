1b4f3b6a749ff1d435a26bc099bb16e5bb6c5109
Juan David Castro <me@juandc.co>
2023-02-01 13:53:24 -0500

    New Challenge: Parenthesis, Algoritmos Avanzados: Lineales (post class)

---
+++ src/challenges/py/proalgoritmoslineales-05/src/main.py
@@ -0,0 +1,16 @@
+def isValid(s):
+   # Tu código aquí 👇
+   return True
+   
+
+response = isValid('()()()')
+print(response)
+
+response = isValid(')(')
+print(response)
+
+response = isValid('((()())())')
+print(response)
+
+response = isValid('(1 + 2) * (3-4')
+print(response)
