7ea6a784d3b44c9c6a72a20d6f0c7a4edf3a216c
Juan David Castro <me@juandc.co>
2023-02-02 08:05:33 -0500

    Solution to Reorganize Str, Algoritmos Avanzados: Lineales

--- src/challenges/py/proalgoritmoslineales-07/src/main.py
+++ src/challenges/py/proalgoritmoslineales-07/src/main.py
@@ -2,7 +2,38 @@ import heapq
 from collections import defaultdict
 
 def reorganizeString(S: str) -> str:
-  return S
+  ocurrenciasPorCaracter = defaultdict(int)
+
+  for caracter in S:
+    ocurrenciasPorCaracter[caracter] += 1
+    if ocurrenciasPorCaracter[caracter] > (len(S)+1) / 2: return ""
+  
+  colaPrioridad = []
+
+  for caracter, ocurrencias in ocurrenciasPorCaracter.items():
+    colaPrioridad.append((-ocurrencias, caracter))
+
+  heapq.heapify(colaPrioridad)
+
+  stringOrdenada = []
+
+  while 2 <= len(colaPrioridad):
+    ocurrencia1, caracter1 = heapq.heappop(colaPrioridad)
+    ocurrencia2, caracter2 = heapq.heappop(colaPrioridad)
+
+    stringOrdenada.append(caracter1)
+    stringOrdenada.append(caracter2)
+
+    if ocurrencia1 + 1:
+      heapq.heappush(colaPrioridad, (ocurrencia1 +1, caracter1))
+
+    if ocurrencia2 + 1:
+      heapq.heappush(colaPrioridad, (ocurrencia2 +1, caracter2))
+
+  if colaPrioridad:
+    return "".join(stringOrdenada) + colaPrioridad[0][1]
+  
+  return "".join(stringOrdenada)
 
 response = reorganizeString("aacab")
 print(response)
