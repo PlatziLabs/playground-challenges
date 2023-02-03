a3c43f51f3994493194214226189042fe3ac715d
Leonardo Espinoza <leoespinozidk@gmail.com>
2023-02-03 17:54:52 -0600

    New challenge: flightAgenda, challenge 30 dias de js

---
+++ src/challenges/js/30DaysJs-26/src/exercise.js
@@ -0,0 +1,19 @@
+export class Flight {
+  // Tu código aquí 👈
+}
+
+export class Passenger {
+  // Tu código aquí 👈
+}
+
+export class Reservation {
+  // Tu código aquí 👈
+}
+
+export class PremiumFlight extends Flight {
+  // Tu código aquí 👈
+}
+
+export class EconomicFlight extends Flight {
+  // Tu código aquí 👈
+}
