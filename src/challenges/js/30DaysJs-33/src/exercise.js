b9ba998b732e88fdd629bd47e542bbcba59df387
Leonardo Espinoza <leoespinozidk@gmail.com>
2023-02-09 00:46:07 -0600

    new challenge: MyArray, challenge, 30 dias de js

---
+++ src/challenges/js/30DaysJs-33/src/exercise.js
@@ -0,0 +1,33 @@
+export class MyArray {
+  constructor() {
+    // Tu código aquí 👈
+  }
+
+  map(func) {
+    // Tu código aquí 👈
+  }
+
+  filter(func) {
+    // Tu código aquí 👈
+  }
+
+  push(item) {
+    // Tu código aquí 👈
+  }
+
+  pop() {
+    // Tu código aquí 👈
+  }
+
+  join(character = ",") {
+    // Tu código aquí 👈
+  }
+
+  shift() {
+    // Tu código aquí 👈
+  }
+
+  unshift(item) {
+    // Tu código aquí 👈
+  }
+}
