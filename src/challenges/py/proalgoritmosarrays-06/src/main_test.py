5aec05241f850819a460faa91911e56e8c2f33e0
Juan David Castro <me@juandc.co>
2023-02-10 17:07:12 -0500

    Add more tests

--- src/challenges/py/proalgoritmosarrays-06/src/main_test.py
+++ src/challenges/py/proalgoritmosarrays-06/src/main_test.py
@@ -9,7 +9,7 @@ def reload_module(name):
   return module
 
 
-def test_number_is_in_matriz():
+def test_number_is_in_matrix():
     module = reload_module('main')
     matriz = [
         [1,3,5,7],
@@ -19,8 +19,17 @@ def test_number_is_in_matriz():
     rta = module.searchInMatrix(matriz, 3)
     assert rta == True
 
+def test_number_is_at_end_of_matrix():
+    module = reload_module('main')
+    matriz = [
+        [2,4,6,8],
+        [10,12,14,16],
+        [18,20,22,24],
+    ]
+    rta = module.searchInMatrix(matriz, 24)
+    assert rta == True
 
-def test_number_not_in_matriz():
+def test_number_not_in_matrix():
     module = reload_module('main')
     matriz = [
         [1,3,5,7],
@@ -28,4 +37,14 @@ def test_number_not_in_matriz():
         [23,30,34,60],
     ]
     rta = module.searchInMatrix(matriz, 12)
+    assert rta == False
+
+def test_number_is_greater_than_last_number_of_matrix():
+    module = reload_module('main')
+    matriz = [
+        [1,3,5,7],
+        [10,11,16,20],
+        [23,30,34,60],
+    ]
+    rta = module.searchInMatrix(matriz, 80)
     assert rta == False
