1e1395e6dd8003384529613fd38b98beb2a0ccf4
Leonardo Espinoza <leoespinozidk@gmail.com>
2023-02-10 15:22:38 -0600

    improve tests

--- src/challenges/js/30DaysJs-17/src/exercise.test.js
+++ src/challenges/js/30DaysJs-17/src/exercise.test.js
@@ -22,4 +22,38 @@ describe("tests", () => {
     const rta = sortByAvailabilityAndPrice(products);
     expect(rta).not.toBe(products);
   });
+
+  it("should sort different products by availability and price", () => {
+    const books = [
+      { name: "book1", price: 200, inStock: true },
+      { name: "book4", price: 5, inStock: false },
+      { name: "book2", price: 20, inStock: false },
+      { name: "book3", price: 150, inStock: true },
+    ];
+
+    const rta = sortByAvailabilityAndPrice(books);
+    expect(rta).toEqual([
+      { name: "book3", price: 150, inStock: true },
+      { name: "book1", price: 200, inStock: true },
+      { name: "book4", price: 5, inStock: false },
+      { name: "book2", price: 20, inStock: false },
+    ]);
+  });
+
+  it("should sort different components by availability and price", () => {
+    const components = [
+      { name: "CPU", price: 300, inStock: true },
+      { name: "Mouse", price: 80, inStock: true },
+      { name: "GPU", price: 505, inStock: false },
+      { name: "Keyboard", price: 120, inStock: true },
+    ];
+
+    const rta = sortByAvailabilityAndPrice(components);
+    expect(rta).toEqual([
+      { name: "Mouse", price: 80, inStock: true },
+      { name: "Keyboard", price: 120, inStock: true },
+      { name: "CPU", price: 300, inStock: true },
+      { name: "GPU", price: 505, inStock: false },
+    ]);
+  });
 });
