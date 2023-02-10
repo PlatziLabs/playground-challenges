e8a7e753a6cfe9a6c575b34c42f0b975d43bdf57
Juan David Castro <me@juandc.co>
2023-02-10 12:16:41 -0500

    Solution to Reorder List Without Repeated Values, Algoritmos Avanzados: Lineales 4 (post class) (#178)
    * New Challenge: RLWRV, Algoritmos Avanzados: Lineales (post class)
    
    * Solution to RLWRV, Algoritmos Avanzados: Lineales (post class)
    
    ---------
    
    Co-authored-by: Nicolas Molina Monroy <hi@nicobytes.com>

--- src/challenges/js/proalgoritmoslineales-04/src/exercise.js
+++ src/challenges/js/proalgoritmoslineales-04/src/exercise.js
@@ -6,8 +6,52 @@ export class Nodo {
 }
 
 export function reorderList(cabeza) {
-  // Tu código aquí 👇
+  if (!cabeza?.siguiente?.siguiente) {
+    return cabeza;
+  }
+  
+  let p1 = cabeza;
+  let p2 = reversarLista(encontrarMitad(clon(cabeza)));
+
+  while (p2) {
+    if (p1.valor == p2.valor) {
+      p1.siguiente = null;
+      p2 = null;
+    } else {
+      let tmp = p1.siguiente;
+      p1.siguiente = p2;
+      p1 = p2;
+      p2 = tmp;
+    }
+  }
+
   return cabeza;
+}
+
+function encontrarMitad(cabeza) {
+  let rapido = cabeza;
+  let lento = cabeza;
+
+  while(rapido && rapido.siguiente) {
+    lento = lento.siguiente;
+    rapido = rapido.siguiente.siguiente;
+  }
+
+  return lento;
+}
+
+function reversarLista(cabeza) {
+  let anterior = null;
+  let actual = cabeza;
+
+  while (actual) {
+    let siguienteTemporal = actual.siguiente;
+    actual.siguiente = anterior;
+    anterior = actual;
+    actual = siguienteTemporal;
+  }
+
+  return anterior;
 }
 
 function clon(obj) {
