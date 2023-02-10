7354122dd663978c20699bce9943bf4e148cb9f7
Juan David Castro <me@juandc.co>
2023-02-10 15:35:26 -0500

    Add more tests

--- src/challenges/py/proalgoritmoslineales-06/src/main_test.py
+++ src/challenges/py/proalgoritmoslineales-06/src/main_test.py
@@ -21,3 +21,15 @@ def test_2_closest_points():
     rta = module.kClosest(tests_list, 2)
     assert [-5, 5] in rta and [13, -8] in rta and [1,20] not in rta
 
+def test_2_surprise_closest_points():
+    module = reload_module('main')
+    tests_list = [[20,-18], [5,25], [-15,50]]
+    rta = module.kClosest(tests_list, 2)
+    assert [5, 25] in rta and [20, -18] in rta and [-15,50] not in rta
+
+def test_3_closest_points():
+    module = reload_module('main')
+    tests_list = [[250,250], [499,1], [-50,450], [390,120]]
+    rta = module.kClosest(tests_list, 3)
+    assert [250,250] in rta and [390,120] in rta and [-50,450] in rta and [499,1] not in rta
+
