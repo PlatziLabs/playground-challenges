50a21cd79559a1e04973fb66ed5f0389a2726262
Nicolas Molina Monroy <hi@nicobytes.com>
2023-02-14 15:54:14 -0400

    :construction: create solution

--- src/challenges/py/numpy-101/src/main.py
+++ src/challenges/py/numpy-101/src/main.py
@@ -1,8 +1,16 @@
 # Import NumPy
-import ___
+import numpy as np
 
 # Convert sudoku_list into an array
-sudoku_array = ___
+sudoku_array = np.array([[0, 0, 4, 3, 0, 0, 2, 0, 9],
+ [0, 0, 5, 0, 0, 9, 0, 0, 1],
+ [0, 7, 0, 0, 6, 0, 0, 4, 3],
+ [0, 0, 6, 0, 0, 2, 0, 8, 7],
+ [1, 9, 0, 0, 0, 7, 4, 0, 0],
+ [0, 5, 0, 0, 8, 3, 0, 0, 0],
+ [6, 0, 0, 0, 0, 0, 1, 0, 5],
+ [0, 0, 3, 5, 0, 8, 6, 9, 0],
+ [0, 4, 2, 9, 1, 0, 3, 0, 0]])
 
 # Print the type of sudoku_array 
-print(___)
+print(type(sudoku_array))
