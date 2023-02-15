d537fa2f55a93d8b0d9119e7d5555c2cb67f3c6b
Juan David Castro <me@juandc.co>
2023-02-15 09:35:39 -0500

    New Challenge: K Closest Points to Origin, Algoritmos Avanzados: Lineales 6 (#173)
    * New Challenge: KCPtO, Algoritmos Avanzados: Lineales
    
    * Add more tests

---
+++ src/challenges/py/proalgoritmoslineales-06/src/main.py
@@ -0,0 +1,13 @@
+import heapq
+from typing import List
+
+def kClosest(puntos: List[List[int]], K: int) -> List[List[int]]:
+   # Tu código aquí 👇
+   return puntos
+
+response = kClosest([[1,3], [3,4], [5,6]], 1)
+print(response)
+
+response = kClosest([[13,-8], [1,20], [-5,5]], 2)
+print(response)
+
