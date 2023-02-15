a14609a1df7d776f87422424e408e8e2ebe14934
Juan David Castro <me@juandc.co>
2023-02-15 10:33:54 -0500

    Remove Solution

--- src/challenges/py/proalgoritmoslineales-05/src/main.py
+++ src/challenges/py/proalgoritmoslineales-05/src/main.py
@@ -1,17 +1,5 @@
 def isValid(s):
-   open = 0
-   close = 0
-
-   for x in s:
-      if x == '(':
-         open += 1
-      if x == ')':
-         if close < open:
-            close += 1
-         else:
-            return False
-   
-   return open == close
+   return True
    
 
 response = isValid('()()()')
