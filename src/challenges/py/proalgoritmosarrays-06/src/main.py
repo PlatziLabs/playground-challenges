90ba4f640bf51fe4780df128b9d0ef5662878b2c
Juan David Castro <me@juandc.co>
2023-02-22 17:58:21 -0500

    New Challenge: Search 2D Array Matrix, Algoritmos Avanzados: Arrays 6  🙏 (#185)
    * New Challenge: S2DAM, Algoritmos Avanzados: Arrays
    
    * Update README.md
    
    * Add more tests

---
+++ src/challenges/py/proalgoritmosarrays-06/src/main.py
@@ -0,0 +1,16 @@
+def searchInMatrix(matriz, objetivo):
+   # Tu código aquí 👇
+   return True
+
+matriz = [
+  [1,3,5,7],
+  [10,11,16,20],
+  [23,30,34,60],
+]
+print(matriz)
+
+response = searchInMatrix(matriz, 3)
+print(response)
+
+response2 = searchInMatrix(matriz, 12)
+print(response2)
