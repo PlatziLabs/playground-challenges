c95dd37afce8d6e95607557b765f6bcd28823b3a
Juan David Castro <me@juandc.co>
2023-02-22 10:19:31 -0500

    ✅ Share connection betweeen setup & tests

--- src/templates/sql/src/exercise.sql
+++ src/templates/sql/src/exercise.sql
@@ -1,9 +1,2 @@
--- INSERT INTO persons (id, name) VALUES (4, "asas");
--- INSERT INTO persons (id, name) VALUES (4, "asas");
-
--- SELECT * FROM persons WHERE id = 1;
-
-INSERT INTO persons (id, name) VALUES (4, "asas");
-INSERT INTO persons (id, name) VALUES (4, "asas");
-
-SELECT id FROM persons;
+-- INSERT INTO persons (id, name) VALUES (4, "Nath");
+-- SELECT id FROM persons;
