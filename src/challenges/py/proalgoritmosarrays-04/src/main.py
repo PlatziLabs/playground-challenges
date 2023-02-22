ce5ec954c2498a69995612dcc94b195ff2ea8dd0
Juan David Castro <me@juandc.co>
2023-02-22 18:26:10 -0500

    New Challenge: Longest Substring Without Repeating Characters, Curso de Algoritmos Avanzados: Arrays 4  🙏 (#189)
    * New Challenge: LSWRC, Curso de Algoritmos Avanzados: Arrays
    
    * Update playground.json
    
    * Remove last test in main
    
    * Update main.py
    
    * Update main.py

---
+++ src/challenges/py/proalgoritmosarrays-04/src/main.py
@@ -0,0 +1,10 @@
+def lengthOfLongestSubstring(s):
+   # Tu código aquí 👇
+   return len(s)
+
+
+response = lengthOfLongestSubstring("abcabcbb")
+print(response)
+
+response2 = lengthOfLongestSubstring("jdkafnlcdsalkxcmpoiuytfccv")
+print(response2)
