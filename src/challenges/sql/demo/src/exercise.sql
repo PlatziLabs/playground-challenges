537f2dfdec843a9f0ddaaf6c9b27012c72fc10f7
Juan David Castro <me@juandc.co>
2023-03-16 21:17:17 -0500

    Tests template: update solutioon

--- src/challenges/sql/demo/src/exercise.sql
+++ src/challenges/sql/demo/src/exercise.sql
@@ -1,3 +1,3 @@
--- INSERT INTO persons VALUES (4, "Nath");
--- SELECT * FROM persons;
--- SELECT * FROM persons WHERE id = 4;
+INSERT INTO persons VALUES (4, "Nath");
+SELECT * FROM persons;
+SELECT * FROM persons WHERE id = 4;
