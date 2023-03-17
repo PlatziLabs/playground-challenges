dc2c92a7a6565eab425e61190bb6e10b6848af5a
Juan David Castro <me@juandc.co>
2023-03-17 15:39:31 -0500

    Fix SQL Template (solution) (#263)
    * fix challenge: task planner, challenge 30 dias de js (#247)
    
    * fix challenge: challenge 30 dias de js 10, task planner
    
    * wip
    
    * fix readme
    
    * SQL README (#249)
    
    * SQL Challenges SQL
    
    * Screenshots
    
    * Update README.md
    
    * improve readme (#250)
    
    * fix readme (#254)
    
    * excercise (#252)
    
    * fix challenge: Encapsula datos de usuarios, challenge 30 dias de js (#256)
    
    * improve readme
    
    * fix typo
    
    * fix tests (#259)
    
    * Tests template: update SQL Demo
    
    * Tests template: update sql template
    
    * Tests template: update readme
    
    * Tests template: update solutioon
    
    ---------
    
    Co-authored-by: Leonardo Espinoza <leoespinozidk@gmail.com>

--- src/challenges/sql/demo/src/utils.py
+++ src/challenges/sql/demo/src/utils.py
@@ -1,34 +1,22 @@
-from importlib import reload, import_module
-import shutil
 import sqlite3
 import sqlparse
 
-connection = sqlite3.connect(":memory:")
+def run_sql(connection):
+  cur = connection.cursor()
 
-def reload_module(name):
-  module = import_module(name)
-  shutil.rmtree("__pycache__", ignore_errors=True)
-  reload(module)
-  return module
+  try:
+    setup = open("./setup.sql", "r").read()
+    cur.executescript(setup)
+  except sqlite3.OperationalError:
+    None
 
-def setup():
-    cur = connection.cursor()
-    try:
-        content = open('./setup.sql', 'r').read()
-        cur.executescript(content)
-        connection.commit()
-    except sqlite3.OperationalError:
-        None
-
-def get_output():
-    cur = connection.cursor()
-    exercise = open('./exercise.sql', 'r').read()
-    queries = sqlparse.parse(exercise)
-    output = []
-    for query in queries:
-        if query.get_type() == 'SELECT':
-            rta = cur.execute(str(query)).fetchall()
-            output.append(rta)
-        else:
-            cur.execute(str(query))
-    return output
+  exercise = open("./exercise.sql", "r").read()
+  queries = sqlparse.parse(exercise)
+  output = []
+  for query in queries:
+    if query.get_type() == "SELECT":
+      rta = cur.execute(str(query)).fetchall()
+      output.append(rta)
+    else:
+      cur.execute(str(query))
+  return output
