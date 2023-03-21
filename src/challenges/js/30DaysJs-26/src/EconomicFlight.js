c8bddc8dd4c72db14dba5fc511fd80019269f1a3
Leonardo Espinoza <leoespinozidk@gmail.com>
2023-03-21 13:03:33 -0600

    New challenge: flightAgenda, challenge 30 dias de js (#199)
    * New challenge: flightAgenda, challenge 30 dias de js
    
    * fix
    
    * fix playground.json

---
+++ src/challenges/js/30DaysJs-26/src/EconomicFlight.js
@@ -0,0 +1,8 @@
+import { Flight } from "./Flight";
+import { Reservation } from "./Reservation";
+
+export class EconomicFlight extends Flight {
+  sellTicket(passenger) {
+    // Tu código aquí 👇
+  }
+}
---
+++ src/challenges/js/30DaysJs-26/src/Flight.js
@@ -0,0 +1,11 @@
+import { Reservation } from "./Reservation";
+
+export class Flight {
+  constructor(origin, destination, date, capacity, price) {
+    // Tu código aquí 👈
+  }
+
+  sellTicket(passenger) {
+    // Tu código aquí 👈
+  }
+}
