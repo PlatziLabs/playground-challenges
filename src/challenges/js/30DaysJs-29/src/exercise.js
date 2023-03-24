2f8f5bc0307215f8787814f0dbef9667820190e4
Leonardo Espinoza <leoespinozidk@gmail.com>
2023-03-24 08:31:37 -0600

    new challenge: decorator in store, challenge 30 dias de js (#213)
    * new challenge: decorator in store, challenge 30 dias de js
    
    * separate files and improve readme

---
+++ src/challenges/js/30DaysJs-29/src/exercise.js
@@ -0,0 +1,11 @@
+import { Product } from "./product";
+
+export class BasicProduct extends Product {
+  constructor(price, description) {
+    // Tu código aquí 👈
+  }
+
+  getDescription() {
+    // Tu código aquí 👈
+  }
+}
