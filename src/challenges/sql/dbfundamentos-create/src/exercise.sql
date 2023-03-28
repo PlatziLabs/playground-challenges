f2d87ce964a7d828d0f2fb20f2949f0b96204e08
Alexis <ala1289@gmail.com>
2023-03-28 15:18:35 -0600

    New Challenge: CREATE TABLE, Fundamentos DBs solution

--- src/challenges/sql/dbfundamentos-create/src/exercise.sql
+++ src/challenges/sql/dbfundamentos-create/src/exercise.sql
@@ -1,1 +1,8 @@
 -- Inserta tu sentencia aqui
+CREATE TABLE IF NOT EXISTS people (
+person_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+last_name VARCHAR(255) NULL,
+first_name VARCHAR(255) NULL,
+address VARCHAR(255) NULL,
+city VARCHAR(255) NULL
+);
