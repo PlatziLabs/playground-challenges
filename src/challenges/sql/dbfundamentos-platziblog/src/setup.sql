a6efddff1dd0adc851ac2b9ce7e71a61f9471c55
Juan David Castro <me@juandc.co>
2023-03-29 15:48:28 -0500

    Update setup.sql

--- src/challenges/sql/dbfundamentos-platziblog/src/setup.sql
+++ src/challenges/sql/dbfundamentos-platziblog/src/setup.sql
@@ -2,7 +2,6 @@
 CREATE TABLE usuarios (
   id int(11) NOT NULL,
   login varchar(30) NOT NULL,
-  password varchar(32) NOT NULL,
   nickname varchar(40) NOT NULL,
   email varchar(40) NOT NULL,
   PRIMARY KEY (id),
