8cf9dd25dbbdbd7e4330f21a5b96d95c7af89812
Juan David Castro <me@juandc.co>
2023-03-30 14:19:04 -0500

    Solution: IUD, MariaDB

--- src/challenges/sql/mariadb-iud/src/exercise.sql
+++ src/challenges/sql/mariadb-iud/src/exercise.sql
@@ -1,2 +1,12 @@
 -- Escribe tu código aquí 👇
+INSERT INTO lines (id, name, color)
+VALUES (7, "Nueva Linea 7", "Naranja");
+
+UPDATE lines
+SET name = "Linea 1"
+WHERE id = 1;
+
+DELETE FROM lines
+WHERE id = 5;
+
 SELECT * FROM lines;
