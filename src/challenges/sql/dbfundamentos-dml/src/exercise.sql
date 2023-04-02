63e177870231652a767a3bd3ba0567f68a59fd57
Alexis <ala1289@gmail.com>
2023-04-01 23:11:11 -0600

    Add solution

--- src/challenges/sql/dbfundamentos-dml/src/exercise.sql
+++ src/challenges/sql/dbfundamentos-dml/src/exercise.sql
@@ -1,1 +1,5 @@
 -- Escribe aquí tu código SQL 👇
+SELECT * FROM students;
+INSERT INTO students (nombre, apellido, edad, correo_electronico, telefono) VALUES ('Alexis', 'Araujo', 33, 'alexis@gmail.com', '777-1234');
+DELETE FROM students where id = 1;
+UPDATE students SET correo_electronico = 'alexis@gmail.com' WHERE id = 2;
