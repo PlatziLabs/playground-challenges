8088eb1469023d9ff651d65bb3349f436af89cae
Leonardo Espinoza <leoespinozidk@gmail.com>
2023-04-03 16:51:58 -0600

    Solution to ContactList, challenge 30 dias de js ✅ (#228)
    * Solution to ContactList, challenge 30 dias de js
    
    * fix readme
    
    * update file
    
    ---------
    
    Co-authored-by: Nicolas Molina Monroy <hi@nicobytes.com>

--- src/challenges/js/30DaysJs-34/src/exercise.js
+++ src/challenges/js/30DaysJs-34/src/exercise.js
@@ -1,6 +1,7 @@
 export class ContactList {
   constructor(size) {
-    // Tu código aquí 👈
+    this.buckets = new Array(size);
+    this.numBuckets = this.buckets.length;
   }
 
   hash(name) {
@@ -12,18 +13,50 @@ export class ContactList {
   }
 
   insert(name, phone) {
-    // Tu código aquí 👈
+    const index = this.hash(name);
+    if (!this.buckets[index]) {
+      this.buckets[index] = [];
+    }
+
+    this.buckets[index].push([name, phone]);
   }
 
   get(name) {
-    // Tu código aquí 👈
+    const index = this.hash(name);
+    if (!this.buckets[index]) {
+      return null;
+    }
+    for (let i = 0; i < this.buckets[index].length; i++) {
+      if (this.buckets[index][i][0] === name) {
+        return this.buckets[index][i][1];
+      }
+    }
+    return null;
   }
 
   retrieveAll() {
-    // Tu código aquí 👈
+    const allValues = [];
+    for (let i = 0; i < this.numBuckets; i++) {
+      if (this.buckets[i]) {
+        for (let j = 0; j < this.buckets[i].length; j++) {
+          allValues.push(this.buckets[i][j]);
+        }
+      }
+    }
+    return allValues;
   }
 
   delete(name) {
-    // Tu código aquí 👈
-  }
+    const index = this.hash(name);
+
+    if (!this.buckets[index]) {
+      return null;
+    }
+
+    for (let i = 0; i < this.buckets[index].length; i++) {
+      if (this.buckets[index][i][0] === name) {
+        this.buckets[index].splice(i, 1);
+        return;
+      }
+    }
 }
