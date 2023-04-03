ab7cab49b30fa78174093e4d0117076fb076a98b
Leonardo Espinoza <leoespinozidk@gmail.com>
2023-04-03 16:45:36 -0600

    solution tu MyArray, challenge 30 dias de js ✅ (#226)
    * solution tu MyArray, challenge 30 dias de js
    
    * work
    
    * update files
    
    ---------
    
    Co-authored-by: Nicolas Molina Monroy <hi@nicobytes.com>

--- src/challenges/js/30DaysJs-33/src/exercise.js
+++ src/challenges/js/30DaysJs-33/src/exercise.js
@@ -1,33 +1,91 @@
 export class MyArray {
   constructor() {
-    // Tu código aquí 👈
+    this.data = {};
+    this.length = 0;
   }
 
   map(func) {
-    // Tu código aquí 👈
+    const newArr = new MyArray();
+    for (let i = 0; i < this.length; i++) {
+      const element = this.data[i];
+      newArr.push(func(element));
+    }
+
+    return newArr;
   }
 
   filter(func) {
-    // Tu código aquí 👈
+    const newArr = new MyArray();
+    for (let i = 0; i < this.length; i++) {
+      const element = this.data[i];
+      if (func(element)) {
+        newArr.push(element);
+      }
+    }
+
+    return newArr;
   }
 
   push(item) {
-    // Tu código aquí 👈
+    this.data[this.length] = item;
+    this.length++;
   }
 
   pop() {
-    // Tu código aquí 👈
+    this.length--;
+    const deletedItem = this.data[this.length];
+
+    delete this.data[this.length];
+
+    return deletedItem;
   }
 
   join(character = ",") {
-    // Tu código aquí 👈
+    let string = this.data[0];
+
+    for (let i = 1; i < this.length; i++) {
+      const element = this.data[i];
+
+      string = `${string}${character}${element}`;
+    }
+
+    return string;
   }
 
   shift() {
-    // Tu código aquí 👈
+    const deletedItem = this.data[0];
+
+    delete this.data[0];
+
+    this.length--;
+    for (let i = 0; i < this.length; i++) {
+      const nextElement = this.data[i + 1];
+      this.data[i] = nextElement;
+    }
+
+    delete this.data[this.length];
+
+    return deletedItem;
   }
 
   unshift(item) {
-    // Tu código aquí 👈
+    if (!item && item !== 0) {
+      return this.length;
+    }
+
+    if (this.length === 0) {
+      this.data[0] = item;
+      this.length++;
+      return this.length;
+    }
+
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+
+    this.data[0] = item;
+
+    this.length++;
+    return this.length;
   }
 }
