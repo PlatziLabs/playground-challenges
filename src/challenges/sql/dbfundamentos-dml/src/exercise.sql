01b97afdbf8bd4880fe27b07ca5340b53b7d53b4
Juan David Castro <me@juandc.co>
2023-04-03 10:33:26 -0500

    Change comillas

--- src/challenges/sql/dbfundamentos-dml/src/exercise.sql
+++ src/challenges/sql/dbfundamentos-dml/src/exercise.sql
@@ -3,10 +3,10 @@ SELECT * FROM students;
 
 INSERT INTO students
 (nombre, apellido, edad, correo_electronico, telefono)
-VALUES ('Alexis', 'Araujo', 33, 'alexis@gmail.com', '777-1234');
+VALUES ("Alexis", "Araujo", 33, "alexis@gmail.com", "777-1234");
 
 DELETE FROM students where id = 1;
 
 UPDATE students
-SET correo_electronico = 'alexis@gmail.com'
+SET correo_electronico = "alexis@gmail.com"
 WHERE id = 2;
