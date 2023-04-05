43ba6cad7478459a959e80f9f71358e0e150df83
alarcon7a <alarcon7a@gmail.com>
2023-04-05 12:08:59 -0500

    fix json

--- src/challenges/py/numpy-102/src/main_test.py
+++ src/challenges/py/numpy-102/src/main_test.py
@@ -1,7 +1,16 @@
 import numpy as np
 from main import crear_array
+from importlib import reload, import_module
+import shutil
+
+def reload_module(name):
+  module = import_module(name)
+  shutil.rmtree("__pycache__", ignore_errors=True)
+  reload(module)
+  return module
 
 def test_crear_array():
+    module = reload_module('main')    
     lista = [1, 2, 3, 4]
     respuesta = crear_array(lista)
     assert isinstance(respuesta, np.ndarray), 'La función no retorna un numpy array.'
