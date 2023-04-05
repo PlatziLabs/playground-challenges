806733c56c0b64f39fa59e0a66505b1f70294d77
alarcon7a <alarcon7a@gmail.com>
2023-04-05 12:11:07 -0500

    fix json

--- src/challenges/py/numpy-102/src/main_test.py
+++ src/challenges/py/numpy-102/src/main_test.py
@@ -12,7 +12,7 @@ def reload_module(name):
 def test_crear_array():
     module = reload_module('main')    
     lista = [1, 2, 3, 4]
-    respuesta = crear_array(lista)
+    respuesta = module.crear_array(lista)
     assert isinstance(respuesta, np.ndarray), 'La función no retorna un numpy array.'
     assert np.all(respuesta == lista), 'El array retornado no coincide con la lista original.'
 
