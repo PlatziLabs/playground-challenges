d84ad91877a8057762deca302cac8a2692f3a69a
Leonardo Espinoza <leoespinozidk@gmail.com>
2023-04-11 14:54:19 -0600

    Solution to LinkedListRecharged, challenge 30 dias de JS (#308)
    * Solution to LinkedListRecharged, challenge 30 dias de JS
    
    * improve readme
    
    ---------
    
    Co-authored-by: Nicolas Molina Monroy <hi@nicobytes.com>

--- src/challenges/js/30DaysJs-37/src/exercise.js
+++ src/challenges/js/30DaysJs-37/src/exercise.js
@@ -3,18 +3,92 @@ import { Node } from "./Node";
 
 export class LinkedListRecharged extends LinkedList {
   get(index){
-   // Tu código aquí 👈
+    if(index < 0 || index >= this.length){
+      return null
+    }
+
+    let currentNode = this.head
+    for (let i = 0; i < index; i++) {
+      currentNode = currentNode.next
+    }
+
+    return currentNode.value
   }
 
   insertAt(index, value){
-    // Tu código aquí 👈
+    if(index < 0 || index >= this.length){
+      return null
+    }
+
+    const newNode = new Node(value)
+    if(index === 0){
+      newNode.next = this.head 
+      this.head = newNode
+      if(!this.tail){
+        this.tail = newNode
+      }
+      this.length++
+
+      return newNode
+    }
+    let currentNode = this.head
+    for(let i = 0; i < index - 1; i++){
+      currentNode = currentNode.next
+    }
+
+    newNode.next = currentNode.next
+    currentNode.next = newNode    
+
+    if(!newNode.next){
+      this.tail = newNode
+    }
+
+    this.length++
+    return newNode
+
   }
 
   toArray(){
-    // Tu código aquí 👈
+    const result = []
+    let currentNode = this.head
+    while(currentNode){
+      result.push(currentNode.value)
+      currentNode = currentNode.next
+    }
+
+    return result
   }
 
   removeAt(index){
-    // Tu código aquí 👈
+    if(index < 0 || index >= this.length){
+      return null
+    }
+
+    let deletedNode = null
+    if(index === 0){
+      deletedNode = this.head
+      this.head = this.head.next
+      if(this.length === 1){
+        this.tail = null
+      }
+      this.length--
+      return deletedNode.value
+    }
+
+    let currentNode = this.head
+
+    for (let i = 0; i < index - 1; i++) {
+      currentNode = currentNode.next
+    }
+
+    deletedNode = currentNode.next
+    currentNode.next = currentNode.next.next
+
+    if(!currentNode.next){
+      this.tail = currentNode
+    }
+
+    this.length--
+    return deletedNode.value
   }
 }
