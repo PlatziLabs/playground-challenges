0aefc678947382be38631c0857ae33a8e74e00ee
Leonardo Espinoza <leoespinozidk@gmail.com>
2023-04-18 07:06:13 -0600

    New challenge patientList, challenge 30 dias de JS (#310)
    * New challenge patientList, challenge 30 dias de JS
    
    * fix

---
+++ src/challenges/js/30DaysJs-38/src/exercise.js
@@ -0,0 +1,27 @@
+import { Node } from "./Node";
+
+export class PatientList {
+  constructor(beds) {
+    // Tu código aquí 👈🏻
+  }
+
+  addPatient(name, age) {
+    // Tu código aquí 👈🏻
+  }
+
+  removePatient(name) {
+    // Tu código aquí 👈🏻
+  }
+
+  getPatient(name) {
+    // Tu código aquí 👈🏻
+  }
+
+  getPatientList() {
+    // Tu código aquí 👈🏻
+  }
+
+  getAvailableBeds() {
+    // Tu código aquí 👈🏻
+  }
+}
