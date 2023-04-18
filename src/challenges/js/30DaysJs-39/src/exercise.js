7ecfad345c2a0a2791937963cef3e2a3e4bae650
Leonardo Espinoza <leoespinozidk@gmail.com>
2023-04-18 07:15:33 -0600

    New challenge Playlist, challenge 30 dias de JS (#311)

---
+++ src/challenges/js/30DaysJs-39/src/exercise.js
@@ -0,0 +1,19 @@
+import { Node } from "./node";
+
+export class Playlist {
+  constructor() {
+    // Tu código aquí 👈🏻
+  }
+
+  addSong(song) {
+    // Tu código aquí 👈🏻
+  }
+
+  playSong() {
+    // Tu código aquí 👈🏻
+  }
+
+  getPlaylist() {
+    // Tu código aquí 👈🏻
+  }
+}
