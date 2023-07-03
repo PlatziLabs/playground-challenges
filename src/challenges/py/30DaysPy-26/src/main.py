4827cd3672adff91ac9e4c506a940443eedf52ce
Leonardo Espinoza <leoespinozidk@gmail.com>
2023-07-03 00:40:28 -0600

    new challenge taskOrganizer, challenge 30 dias de py

---
+++ src/challenges/py/30DaysPy-26/src/main.py
@@ -0,0 +1,12 @@
+class TaskManager:
+  def __init__(self):
+    # Tu código aquí 👈
+    pass
+
+  def addTask(self, task, tags):
+    # Tu código aquí 👈
+    pass
+
+  def printTasks(self):
+    # Tu código aquí 👈
+    pass
