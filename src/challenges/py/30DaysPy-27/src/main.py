be563bc2de659af0f8e9e46070dae8ea26e0a507
Leonardo Espinoza <leoespinozidk@gmail.com>
2023-07-13 13:04:21 -0600

    new challenge patientList, challenge 30 dias de py

---
+++ src/challenges/py/30DaysPy-27/src/main.py
@@ -0,0 +1,26 @@
+from Node import PatientNode
+
+class PatientList:
+  def __init__(self, max_beds):
+    # Tu código aquí 👇
+    pass
+
+  def addPatient(self, name, age):
+    # Tu código aquí 👇
+    pass
+
+  def removePatient(self, name):
+    # Tu código aquí 👇
+    pass
+
+  def getPatient(self, name):
+    # Tu código aquí 👇
+    pass
+
+  def getPatientList(self):
+    # Tu código aquí 👇
+    pass
+
+  def getAvailableBeds(self):
+    # Tu código aquí 👇
+    pass
