dcf6c333fc81cb97c04421266920cd0bf34f93e2
Leonardo Espinoza <leoespinozidk@gmail.com>
2023-07-13 13:17:15 -0600

    new challenge playlist, challenge 30 dias de py

---
+++ src/challenges/py/30DaysPy-28/src/main.py
@@ -0,0 +1,18 @@
+from node import Node
+
+class Playlist:
+  def __init__(self):
+    # Tu código aquí 👇
+    pass
+
+  def addSong(self, song):
+    # Tu código aquí 👇
+    pass
+
+  def playSong(self):
+    # Tu código aquí 👇
+    pass
+
+  def getPlaylist(self):
+    # Tu código aquí 👇
+    pass
